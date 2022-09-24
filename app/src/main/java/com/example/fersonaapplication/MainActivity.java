283a897cb987c27e27de08f1551f6a0277881fb1
주혜수 <106126676+juhyesu128@users.noreply.github.com>
2022-09-24 16:31:44 +0900

    테스트1

---
+++ app/src/main/java/com/example/fersonaapplication/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.fersonaapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        
+        // 제발 되라 제발 제발 제발 제발
+    }
+}

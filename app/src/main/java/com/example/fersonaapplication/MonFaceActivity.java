64e4185a3adc3975100cd6e54c48bf95ac16cb9e
주혜수 <106126676+juhyesu128@users.noreply.github.com>
2022-09-27 09:25:45 +0900

    신고하기_녹음기능 추가_주혜수

--- app/src/main/java/com/example/fersonaapplication/MonFaceActivity.java
+++ app/src/main/java/com/example/fersonaapplication/MonFaceActivity.java
@@ -1,26 +1,45 @@
 package com.example.fersonaapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MonFaceActivity extends AppCompatActivity implements View.OnClickListener {
 
     ImageView logo;
-    EditText monMake_et;
+    EditText monMake_et; // 여기에 녹음 내용 써지게 하기!
     Button make_btn;
+    Intent intent;
     ImageButton voice_btn;
+    final int PERMISSION = 1;
+    SpeechRecognizer mRecognizer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mon_face);
+
+        // 안드로이드 6.0버전 이상인지 체크해서 퍼미션 체크
+        if (Build.VERSION.SDK_INT >= 23) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.INTERNET,
+                    Manifest.permission.RECORD_AUDIO}, PERMISSION);
+        }
 
         logo = findViewById(R.id.logo_img);
         monMake_et = findViewById(R.id.monMake_et);
@@ -29,8 +48,23 @@ public class MonFaceActivity extends AppCompatActivity implements View.OnClickLi
 
         logo.setOnClickListener(this);
         monMake_et.setOnClickListener(this);
-        voice_btn.setOnClickListener(this);
+//        voice_btn.setOnClickListener(this);
         make_btn.setOnClickListener(this);
+
+        // RecognizerIntent 생성
+        intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, getPackageName()); // 여분의 키
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "ko-KR"); // 언어 설정
+
+        voice_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mRecognizer = SpeechRecognizer.createSpeechRecognizer(MonFaceActivity.this); // 새 SpeechRecognizer 를 만드는 팩토리 메서드
+                mRecognizer.setRecognitionListener(listener); // 리스너 설정
+                mRecognizer.startListening(intent); // 듣기 시작
+            }
+        });
+
     }
 
     @Override
@@ -57,4 +91,98 @@ public class MonFaceActivity extends AppCompatActivity implements View.OnClickLi
                 break;
         }
     }
+
+
+    private RecognitionListener listener = new RecognitionListener() {
+        @Override
+        public void onReadyForSpeech(Bundle params) {
+            // 말하기 시작할 준비가되면 호출
+            Toast.makeText(getApplicationContext(),"음성인식 시작",Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onBeginningOfSpeech() {
+            // 말하기 시작했을 때 호출
+        }
+
+        @Override
+        public void onRmsChanged(float rmsdB) {
+            // 입력받는 소리의 크기를 알려줌
+        }
+
+        @Override
+        public void onBufferReceived(byte[] buffer) {
+            // 말을 시작하고 인식이 된 단어를 buffer에 담음
+        }
+
+        @Override
+        public void onEndOfSpeech() {
+            // 말하기를 중지하면 호출
+        }
+
+        @Override
+        public void onError(int error) {
+            // 네트워크 또는 인식 오류가 발생했을 때 호출
+            String message;
+
+            switch (error) {
+                case SpeechRecognizer.ERROR_AUDIO:
+                    message = "오디오 에러";
+                    break;
+                case SpeechRecognizer.ERROR_CLIENT:
+                    message = "클라이언트 에러";
+                    break;
+                case SpeechRecognizer.ERROR_INSUFFICIENT_PERMISSIONS:
+                    message = "퍼미션 없음";
+                    break;
+                case SpeechRecognizer.ERROR_NETWORK:
+                    message = "네트워크 에러";
+                    break;
+                case SpeechRecognizer.ERROR_NETWORK_TIMEOUT:
+                    message = "네트웍 타임아웃";
+                    break;
+                case SpeechRecognizer.ERROR_NO_MATCH:
+                    message = "찾을 수 없음";
+                    break;
+                case SpeechRecognizer.ERROR_RECOGNIZER_BUSY:
+                    message = "RECOGNIZER 가 바쁨";
+                    break;
+                case SpeechRecognizer.ERROR_SERVER:
+                    message = "서버가 이상함";
+                    break;
+                case SpeechRecognizer.ERROR_SPEECH_TIMEOUT:
+                    message = "말하는 시간초과";
+                    break;
+                default:
+                    message = "알 수 없는 오류임";
+                    break;
+            }
+
+            Toast.makeText(getApplicationContext(), "에러 발생 : " + message,Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onResults(Bundle results) {
+            // 인식 결과가 준비되면 호출
+            // 말을 하면 ArrayList에 단어를 넣고 textView에 단어를 이어줌
+            ArrayList<String> matches =
+                    results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+
+            for(int i = 0; i < matches.size() ; i++){
+//                textView.setText(matches.get(i));
+                monMake_et.setText(matches.get(i));
+            }
+        }
+
+        @Override
+        public void onPartialResults(Bundle partialResults) {
+            // 부분 인식 결과를 사용할 수 있을 때 호출
+        }
+
+        @Override
+        public void onEvent(int eventType, Bundle params) {
+            // 향후 이벤트를 추가하기 위해 예약
+        }
+    };
+
 }
